d483a49b9297f4fac558a38b917012b58add1881
NickTsalyn <129570585+NickTsalyn@users.noreply.github.com>
2024-02-01 10:30:55 -0500

    create architecture

---
+++ src/Pages/ScreenPage.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const ScreenPage = () => {
+  return (
+    <div>ScreenPage</div>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { App } from "App";
+import "./index.css";
+import { Provider } from "react-redux";
+import { store, persistor } from "redux/store";
+import { BrowserRouter } from "react-router-dom";
+import { PersistGate } from "redux-persist/integration/react";
+import { Loader } from 'components/Loader/Loader';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<Provider store={store}>
+			<PersistGate persistor={persistor} loading={<Loader/>}>
+				<BrowserRouter basename="/">
+					<App />
+				</BrowserRouter>
+			</PersistGate>
+		</Provider>
+	</React.StrictMode>
 );
