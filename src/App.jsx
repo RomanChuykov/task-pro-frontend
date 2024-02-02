b6a440963d2eb95c03279326d00140370eed71ba
KaterynaSch <128621831+KaterynaSch@users.noreply.github.com>
2024-02-02 18:39:44 +0200

    added-routes-and-layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
 import { WelcomePage } from 'Pages/WelcomePage';
-import { Layout } from 'components/Layout/Layout';
+import { HomeLayout, WelcomeLayout } from 'components/Layouts/Layouts';
 import { Route, Routes } from 'react-router-dom';
 
 export const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<WelcomePage />} />
-        {/* <Route path="/auth/:id">
-          <Route path="/auth/register" />
-          <Route path="/auth/login" />
-          <Route />
+      <Route path="/" element={<WelcomeLayout />}>
+        <Route path ="/welcome" element={<WelcomePage />} />
+        <Route path="/auth/:id" element={<h2>AuthPage</h2>}>
+          <Route path="register" element={<h2>Registration</h2>}/>
+          <Route path="login" element={<h2>LogIn</h2>}/>
         </Route>
-        <Route path="/home">
-          <Route path="/home/:boardName" />
-        </Route> */}
+      </Route>      
+	    <Route path="/home"  element={<HomeLayout />}>
+        <Route index element={<h2>HomePage</h2>} >
+          <Route path="home/:boardName" element={<h2>ScreensPage</h2>}/>
+        </Route> 
       </Route>
     </Routes>
   );
