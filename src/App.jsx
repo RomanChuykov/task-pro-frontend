0440eaa38a626e20a70f9759ae4e2ebe262c1190
Elena Berezina <farfalina1908@gmail.com>
2024-02-02 17:45:03 +0200

    Add Column Modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,25 @@
 import { WelcomePage } from 'Pages/WelcomePage';
 import { Layout } from 'components/Layout/Layout';
 import { Route, Routes } from 'react-router-dom';
+import {AddColumnButton} from './components/Button/AddColumnButton'
 
 export const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<WelcomePage />} />
-        {/* <Route path="/auth/:id">
-          <Route path="/auth/register" />
-          <Route path="/auth/login" />
-          <Route />
-        </Route>
-        <Route path="/home">
-          <Route path="/home/:boardName" />
-        </Route> */}
-      </Route>
-    </Routes>
+    
+<AddColumnButton />
+
+    // <Routes>
+    //   <Route path="/" element={<Layout />}>
+    //     <Route index element={<WelcomePage />} />
+    //     {/* <Route path="/auth/:id">
+    //       <Route path="/auth/register" />
+    //       <Route path="/auth/login" />
+    //       <Route />
+    //     </Route>
+    //     <Route path="/home">
+    //       <Route path="/home/:boardName" />
+    //     </Route> */}
+    //   </Route>
+    // </Routes>
   );
 };
