536cc7c7264d3a0cb064c9c303c4b9db75182132
Elena Berezina <farfalina1908@gmail.com>
2024-02-02 19:16:51 +0200

    reduct add modal

--- src/components/Button/AddColumnButton.jsx
+++ src/components/Button/AddColumnButton.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { AddColumnModal } from "../ColumnModal/AddColumnModal/AddColumnModal";
 import { EditColumnModal } from "../ColumnModal/EditColumnModal/EditColumnModal";
+import { Button } from 'components/Button/Button';
+
 
 import "./AddColumnButton.css";
 
@@ -8,6 +10,8 @@ export const AddColumnButton = () => {
   const [modalAddColumnIsOpen, setModalAddColumnIsOpen] = useState(false);
   const [modalEditColumnIsOpen, setModalEditColumnIsOpen] = useState(false);
   
+  const handlerTitleColumn = evt => {
+    evt.preventDefault();}
 
   return (
     <main className="main">
@@ -31,20 +35,60 @@ export const AddColumnButton = () => {
         <AddColumnModal
           isOpen={modalAddColumnIsOpen}
           onClose={() => setModalAddColumnIsOpen(false)}
-          submitButton="Спасибо"
+          submitButton="Add"
         >
-          <h2>Поздравляем!</h2>
-          <p>Вы выиграли уникальный смартфон</p>
+
+<div className="container">
+      <p className="addColumn_title">Add column</p>
+      <form onSubmit={handlerTitleColumn}>
+        <input
+          type="text"
+          name="title"
+          placeholder="Title"
+          className="addColumn_input"
+          autoFocus
+        />
+        <Button type="submit" className="addColumn_btn">
+          <span className="iconWrap">
+            {/* <Icon id={'icon-plus'} className={css.icon} /> */}
+          </span>
+          Add
+        </Button>
+      </form>
+    </div>
+
+          {/* <h2>Поздравляем!</h2>
+          <p>Вы выиграли уникальный смартфон</p> */}
           
         </AddColumnModal>
 
         <EditColumnModal
           isOpen={modalEditColumnIsOpen}
           onClose={() => setModalEditColumnIsOpen(false)}
-          submitButton="Ок"
+          submitButton="Add"
         >
-          <h2>Ваше сообщение отправлено</h2>
-          <p>Мы свяжемся с вами в течение двух рабочих дней</p>
+ <div className="container">
+      <p className="addColumn_title">Edit column</p>
+      <form onSubmit={handlerTitleColumn}>
+        <input
+          type="text"
+          name="title"
+          placeholder="To Do"
+          className="addColumn_input"
+          autoFocus
+        />
+        <Button type="submit" className="addColumn_btn">
+          <span className="iconWrap">
+            {/* <Icon id={'icon-plus'} className={css.icon} /> */}
+          </span>
+          Add
+        </Button>
+      </form>
+    </div>
+
+          {/* <h2>Ваше сообщение отправлено</h2>
+          <p>Мы свяжемся с вами в течение двух рабочих дней</p> */}
+
         </EditColumnModal> 
 
        
---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,21 @@
+export const Button = ({
+    children,
+    type = 'click',
+    className,
+    handlerClick,
+    label,
+  }) => {
+    // Компонент кнопка, для зміни типу кнопки передаємо проп type="value"-string
+    // якщо необхідно стилізувати кнопку, передаем компоненту <Button className={your style }></Button>
+  
+    return (
+      <button
+        type={type}
+        className={className}
+        onClick={handlerClick}
+        aria-label={label}
+      >
+        {children}
+      </button>
+    );
+  };
