270cbc0d252f21200de1e4265b52daa1dd6d0452
KaterynaSch <128621831+KaterynaSch@users.noreply.github.com>
2024-02-03 22:23:56 +0200

    fixed-layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
+import { HomePage } from 'Pages/HomePage';
 import WelcomePage  from 'Pages/WelcomePage';
-
 import Auth from 'components/Auth/Auth';
-import { Layout } from 'components/Layout/Layout';
+import { HomeLayout, WelcomeLayout } from 'components/Layout/Layout';
 import { PrivateRoute } from 'components/PrivateRoute';
 import RegisterForm from 'components/Register/RegisterForm';
 import { Route, Routes } from 'react-router-dom';
@@ -9,18 +9,21 @@ import { Route, Routes } from 'react-router-dom';
 export const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Layout />}>
+      <Route path="/" element={<WelcomeLayout />}>
         <Route index element={<WelcomePage />} />
+        <Route path="auth/:id" element={<Auth />}>
+          <Route path="register" element={<RegisterForm />} />
+          <Route path="login" element={<>login</>} />
+        </Route>
+        </Route> 
+      <Route path="/home"  element={<HomeLayout />} >
         <Route path="/home" element={
             <PrivateRoute redirectTo="/auth/login" component={<HomePage />} />
           } >
           <Route path="/home/:boardName" />
         </Route>
       </Route>
-      <Route path="auth/:id" element={<Auth />}>
-        <Route path="register" element={<RegisterForm />} />
-        <Route path="login" element={<>login</>} />
-      </Route>
+      
     </Routes>
   );
 };
